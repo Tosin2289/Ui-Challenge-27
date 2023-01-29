55686d7ca97af7062cf62c88fa0e0ae055f08bc9
Tosin2289 <ayoolatosin00@gmail.com>
2023-01-29 23:56:31 +0100

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,76 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            child: Expanded(
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/bg.jpg'), fit: BoxFit.cover)),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Icon(
+                            Icons.arrow_back_ios,
+                            size: 25,
+                          ),
+                          Text(
+                            "Add Event",
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold),
+                          ),
+                          Text("")
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Image.asset(
+                      "assets/donut.png",
+                      height: 150,
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Upload your files here",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          "Browse",
+                          style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.brown[400]),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
